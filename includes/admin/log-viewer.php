77389f362f8a64defa12b044b9cfbaa0f55f1292
wolfesq <ncwwolfe@gmail.com>
2018-08-20 20:19:19 +0100

    Added debug logging

---
+++ includes/admin/log-viewer.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Simple page that shows the debug.log entries (newest first) under the Tools admin submenu.
+ *
+ * @package ConsoleLog
+ */
+
+/**
+ * Create the Debug Log menu item in the WordPress admin page under the tools submenu.
+ */
+function php_console_log_viewer() {
+	add_submenu_page( 'tools.php', 'Log Viewer', 'Debug Log', 'manage_options', 'php-console-log', 'show_php_console_log_viewer' );
+}
+add_action( 'admin_menu', 'php_console_log_viewer' );
+
+
+
+/**
+ * Create the Debug Log page content.
+ */
+function show_php_console_log_viewer() {
+
+	ob_start();
+	?>
+	<div class="wrap">
+		<h2>Debug Log Viewer</h2>
+	</div>
+		<br>
+		<br>
+	<div>
+		<?php
+		$logfile = file_get_contents( WP_CONTENT_DIR . '/debug.log' );
+		$logs    = array_reverse( explode( '[', $logfile ) );
+
+		foreach ( $logs as $log ) {
+			echo '[' . $log . '<br><br>';
+
+		}
+		?>
+	</div>
+	<?php
+	$debug_log_page = ob_get_clean();
+	echo $debug_log_page;
+}
--- php-console-log.php
+++ php-console-log.php
@@ -13,43 +13,78 @@
  * @example console_log( $array , 'error' );
  * @example console_log( $object , 'warning' );
  * @example console_log( "Debug test" , 'debug' ); // Make sure the console is set to show debug or in verbose mode.
+ * @example console_log( array( 'one', 'two', 'three' ), 'warning', true ); // Print array and write it to debug.log if enabled.
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-require dirname( __FILE__ ) . '/class-console.php';
+// Require the Console class file.
+require dirname( __FILE__ ) . '/includes/classes/class-console.php';
 
 /**
- * Translate simplified function to Console class
- *
- * @param *      $info The information to display in the console.
- * @param string $type Whether it is info, error, warning, or debug type in the console.
+ * Include the Debug Log admin page in Tools submenu if debug.log is enabled in wp-config.php.
  */
-function console_log( $info, $type = 'info' ) {
-
-	$output_type = $type;
-
-	if ( 'info' === $type ) {
-		$output_type = Console::INFO;
-
-	} elseif ( 'debug' === $type ) {
-
-		$output_type = Console::DEBUG;
-
-	} elseif ( 'warning' === $type ) {
-			$output_type = Console::WARNING;
-
-	} elseif ( 'error' === $type ) {
-		$output_type = Console::ERROR;
-
-	} else {
-		$output_type = Console::INFO;
-	}
-
-	Console::show( $info, $output_type );
-
+if ( ( true === WP_DEBUG ) && ( true === WP_DEBUG_LOG ) ) {
+	include_once dirname( __FILE__ ) . '/includes/admin/log-viewer.php';
 }
 
-// console_log( array( 'one', 'two', 'three' ), 'warning' ); // test call.
+if ( ! function_exists( 'console_log' ) ) {
+	/**
+	 * Translate simplified function to Console class
+	 *
+	 * @param *      $info The information to display in the console.
+	 * @param string $type Whether it is info, error, warning, or debug type in the console.
+	 * @param bool   $write_it Whether to write the $info to the error log as well.
+	 */
+	function console_log( $info, $type = 'info', $write_it = false ) {
+
+		$output_type = $type;
+
+		if ( 'info' === $type ) {
+			$output_type = Console::INFO;
+
+		} elseif ( 'debug' === $type ) {
+
+			$output_type = Console::DEBUG;
+
+		} elseif ( 'warning' === $type ) {
+				$output_type = Console::WARNING;
+
+		} elseif ( 'error' === $type ) {
+			$output_type = Console::ERROR;
+
+		} else {
+			$output_type = Console::INFO;
+		}
+
+		// Write to the JavaScript console using the Console class.
+		Console::show( $info, $output_type );
+
+		// Write to debug.log if debugging is enabled and $write_it is true.
+		if ( true === $write_it ) {
+			write_log( $info );
+		}
+
+	}
+}
+
+if ( ! function_exists( 'write_log' ) ) {
+	/**
+	 * Record debugging in the error log if debugging is enabled in wp-config.php.
+	 *
+	 * @param string $log The value to write to the log.
+	 */
+	function write_log( $info ) {
+
+		if ( ( true === WP_DEBUG ) && ( true === WP_DEBUG_LOG ) ) {
+
+			if ( is_array( $info ) || is_object( $info ) ) {
+				error_log( print_r( $info, true ) );
+			} else {
+				error_log( $info );
+			}
+		}
+	}
+}
